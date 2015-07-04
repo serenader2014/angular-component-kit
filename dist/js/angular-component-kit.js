18fb5169b484b1e5c1fad17d6d7c63c5ed057609
serenader <xyslive@gmail.com>
2015-07-04 23:56:09 +0800

    add ripple component and update html file

--- dist/js/angular-component-kit.js
+++ dist/js/angular-component-kit.js
@@ -31,3 +31,30 @@ angular.module('ngComponentKit').directive('ckInput', ['$compile', function ($co
         }
     };
 }]);
+angular.module('ngComponentKit').directive('ckRipple', function () {
+    return {
+        link: function (scope, elem, attrs) {
+            elem.addClass('ck-ripple');
+            var ink = elem.find('.ck-ripple-ink');
+            if (attrs.ckRipple !== 'ck-ripple') {
+                var color = attrs.ckRipple;
+                elem.addClass('ck-ripple-' + color);
+            }
+            elem.on('click', function (event) {
+                ink.removeClass('ck-ripple-animate');
+
+                var offsetLeft = elem.offset().left;
+                var offsetTop = elem.offset().top;
+                var width = elem[0].offsetWidth;
+                var height = elem[0].offsetHeight;
+                var inkWidth = Math.max(width, height);
+                var x = event.pageX - offsetLeft - inkWidth/2;
+                var y = event.pageY - offsetTop - inkWidth/2;
+                ink.css({width: inkWidth, height: inkWidth, top: y, left: x});
+                ink.addClass('ck-ripple-animate');
+            });
+        },
+        transclude: true,
+        template: '<span class="ck-ripple-ink"></span><span ng-transclude></span>'
+    };
+});
