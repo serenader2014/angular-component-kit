5b0d08986b8402a684a55ced3a54bb1899981ec5
serenader <xyslive@gmail.com>
2015-07-10 01:02:00 +0800

    update modal component, wip

--- src/app/modal.js
+++ src/app/modal.js
@@ -1,12 +1,12 @@
 angular.module('ngComponentKit').directive('ckModal', function () {
     return {
-        template: ['<div ng-show="show" class="ck-modal"><div ck-drag=".ck-modal-head" class="ck-modal-wrapper">',
+        template: ['<div ng-show="show" class="ck-modal" ck-drag=".ck-modal-head" >',
             '<div class="ck-modal-head">{{title}}</div>',
             '<div class="ck-modal-body" ng-transclude></div>',
             '<div class="ck-modal-foot">',
             '<button ng-click="ok()" class="ck-btn ck-btn-blue" ck-ripple>Comfirm</button>',
             '<button ng-click="off($event)" class="ck-btn" ck-ripple="black">Close</button>',
-            '</div></div></div>'
+            '</div></div>'
             ].join(''),
         transclude: true,
         scope: {
@@ -15,30 +15,41 @@ angular.module('ngComponentKit').directive('ckModal', function () {
             close: '&',
             show: '='
         },
-        link: function (scope, element) {
+        link: function (scope, element, attr) {
             function closeModal () {
                 scope.show = false;
             }
             var doc = angular.element(document);
+            var screenHeight = angular.element(window).height();
+            var screenWidth = angular.element(window).width();
+            if (screenWidth > 800) {
+                scope.$watch('show', function (show) {
+                    if (show) {
+                        element.css({
+                            top: doc.scrollTop() +200,
+                            left: doc.width()/2 - 400
+                        });
+                        element.find('.ck-modal-body').css({
+                            maxHeight: screenHeight - 400
+                        });
+                    }
+                });
+            } else {
+                element.find('.ck-modal-body').css({
+                    maxHeight: screenHeight - 132
+                });
+            }
 
-            scope.$watch('show', function (show) {
-                if (show) {
-                    element.find('.ck-modal-wrapper').css({
-                        top: doc.scrollTop() +200,
-                        left: doc.width()/2 - 400
-                    });
-                }
-            });
 
             scope.ok = function () {
-                if (scope.confirm && typeof scope.confirm === 'function') {
+                if (attr.confirm) {
                     scope.confirm({scope: scope, close: closeModal});
                 } else {
                     closeModal();
                 }
             };
             scope.off = function () {
-                if (scope.close && typeof scope.close === 'function') {
+                if (attr.close) {
                     scope.close({scope: scope, close: closeModal});
                 } else {
                     closeModal();
